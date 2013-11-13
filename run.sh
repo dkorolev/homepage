5b6f2a87d5fcc8ea62b4cea5772043761070a219
Dima Korolev <dmitry.korolev@gmail.com>
2013-11-12 22:59:31 -0800

    Fixing forever + nodemon.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # TODO(dkorolev): 'nodemon' seems to not be able to pass command line parameters. Perhaps fix one day.
 (cd src; npm install)
-(cd src; node_modules/nodemon/nodemon.js --delay 2 ./node_modules/forever/bin/forever server.js)
+(cd src; ./node_modules/forever/bin/forever start node_modules/nodemon/nodemon.js --delay 2 server.js)
