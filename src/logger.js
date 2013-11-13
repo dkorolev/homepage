93304cbc81ee6054d069aec2b0f8d1890ec01765
Dima Korolev <dmitry.korolev@gmail.com>
2013-11-12 23:35:13 -0800

    log format change.

--- src/logger.js
+++ src/logger.js
@@ -3,6 +3,8 @@ var circular_json = require('circular-json');
 
 exports.logger = function(req, res, next) {
   // TODO(dkorolev): Unify this.
-  fs.appendFileSync('debug.txt', circular_json.stringify(req.connection));
+  fs.appendFileSync(
+    'debug.txt',
+    Date.now() + circular_json.stringify(req.connection) + '\n');
   next();
 };
