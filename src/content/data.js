0159914519a63c1953a0200e261de2b5c6bc45c0
Dima Korolev <dmitry.korolev@gmail.com>
2013-11-27 16:21:00 -0800

    Added e-mail.

--- src/content/data.js
+++ src/content/data.js
@@ -3,33 +3,38 @@ module.exports = {
   profiles: [
     {
       name: 'LinkedIn',
-      comment: 'Professional profile',
+      comment: 'Professional profile.',
       url: 'http://www.linkedin.com/in/dimakorolev',
     },
     {
       name: 'Facebook',
-      comment: 'Personal profile',
+      comment: 'Personal profile.',
       url: 'https://www.facebook.com/dima.korolev.16',
     },
     {
       name: 'Medium',
-      comment: 'Blog I write',
+      comment: 'Blog I write.',
       url: 'https://medium.com/@UniqueDima',
     },
     {
       name: 'Quora',
-      comment: 'Questions I ask and answer',
+      comment: 'Questions I ask and answer.',
       url: 'http://www.quora.com/Dima-Korolev',
     },
     {
       name: 'GitHub',
-      comment: 'Code I write',
+      comment: 'Code I write.',
       url: 'https://github.com/dkorolev',
     },
     {
       name: 'Twitter',
-      comment: 'Tweets I find interesting',
+      comment: 'Tweets I find interesting.',
       url: 'https://twitter.com/UniqueDima',
+    },
+    {
+      name: 'e-mail',
+      comment: 'Best way to reach me.',
+      url: 'mailto:dmitry.korolev@gmail.com',
     },
   ],
 };
