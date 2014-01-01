974b31c049a3d7642d7ab39c51ef2d751842f80d
Dima Korolev <dmitry.korolev@gmail.com>
2014-01-01 08:52:31 -0800

    Rearranged sections even more.

--- src/content/data.js
+++ src/content/data.js
@@ -3,16 +3,6 @@ module.exports = {
   google_analytics_code: 'UA-46065883-1',
   profiles: [
     {
-      name: 'LinkedIn',
-      comment: 'Professional profile.',
-      url: 'http://www.linkedin.com/in/dimakorolev',
-    },
-    {
-      name: 'Facebook',
-      comment: 'Personal profile.',
-      url: 'https://www.facebook.com/dima.korolev.16',
-    },
-    {
       name: 'Quora',
       comment: 'Questions I ask and answer.',
       url: 'http://dkorolev.quora.com/My-Answers',
@@ -23,14 +13,24 @@ module.exports = {
       url: 'https://medium.com/dima-korolev',
     },
     {
-      name: 'GitHub',
-      comment: 'Code I write.',
-      url: 'https://github.com/dkorolev',
-    },
-    {
       name: 'Twitter',
       comment: 'Tweets I find interesting.',
       url: 'https://twitter.com/UniqueDima',
+    },
+    {
+      name: 'Facebook',
+      comment: 'Personal profile.',
+      url: 'https://www.facebook.com/dima.korolev.16',
+    },
+    {
+      name: 'LinkedIn',
+      comment: 'Professional profile.',
+      url: 'http://www.linkedin.com/in/dimakorolev',
+    },
+    {
+      name: 'GitHub',
+      comment: 'Code I write.',
+      url: 'https://github.com/dkorolev',
     },
     {
       name: 'e-mail',
