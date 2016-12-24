34e5ca5ab951f3c2ea30b7213368f77d3efd4a59
Dima <dmitry.korolev@gmail.com>
2016-12-24 13:31:57 -0800

    Reorder the items.

--- src/content/data.js
+++ src/content/data.js
@@ -3,24 +3,9 @@ module.exports = {
   google_analytics_code: 'UA-46065883-1',
   profiles: [
     {
-      name: 'Medium',
-      comment: 'Blog I write.',
-      url: 'http://bit.ly/1i3t0ip',
-    },
-    {
-      name: 'Quora',
-      comment: 'Questions I ask and answer.',
-      url: 'http://bit.ly/1iJQcXN',
-    },
-    {
-      name: 'Twitter',
-      comment: 'Tweets I find interesting.',
-      url: 'http://bit.ly/1i3t4ib',
-    },
-    {
-      name: 'Facebook',
-      comment: 'Personal profile.',
-      url: 'http://on.fb.me/1Q7y09G',  // 'http://on.fb.me/1crFcqZ',
+      name: 'GitHub',
+      comment: 'Code I write.',
+      url: 'http://bit.ly/Koowrq',
     },
     {
       name: 'LinkedIn',
@@ -28,14 +13,29 @@ module.exports = {
       url: 'http://linkd.in/1f1IS7k',
     },
     {
+      name: 'Facebook',
+      comment: 'Personal profile.',
+      url: 'http://on.fb.me/1Q7y09G',  // 'http://on.fb.me/1crFcqZ',
+    },
+    {
+      name: 'Medium',
+      comment: 'Blog I have.',
+      url: 'http://bit.ly/1i3t0ip',
+    },
+    {
+      name: 'Twitter',
+      comment: 'Tweets I find interesting.',
+      url: 'http://bit.ly/1i3t4ib',
+    },
+    {
+      name: 'Quora',
+      comment: 'Questions I ask and answer.',
+      url: 'http://bit.ly/1iJQcXN',
+    },
+    {
       name: 'Google+',
       comment: 'Have yet to figure out.',
       url: 'http://bit.ly/1f1IuFV',
-    },
-    {
-      name: 'GitHub',
-      comment: 'Code I write.',
-      url: 'http://bit.ly/Koowrq',
     },
     {
       name: 'e-mail',
